701255674812244a2202c169b886970540ee37e8
Vishan Sonagadu <vishansonagadu@gmail.com>
2024-10-04 19:07:06 -0400

    Initial commit

---
+++ content.js
@@ -0,0 +1,22 @@
+
+function clickSkipButton() {
+    // Look for skip element
+    const skipButton = document.querySelector('button[data-uia="player-skip-recap"]') || document.querySelector('button[data-uia="player-skip-intro"]') || document.querySelector('button[data-uia="next-episode-seamless-button"]');
+  
+    if (skipButton) {
+      // Click the "Skip Intro" button if it's found
+      skipButton.click();
+    }
+  }
+  
+  // MutationObserver to monitor DOM changes
+  const observer = new MutationObserver(() => {
+    clickSkipButton();
+  });
+  
+  // Observe the entire document for changes
+  observer.observe(document, { childList: true, subtree: true });
+  
+ // In case the button is already on screen
+  clickSkipButton();
+  
---
+++ popup/popup.js
@@ -0,0 +1,33 @@
+console.log("popup.js is loaded");  // At the very top of popup.js
+
+console.log('here');
+document.addEventListener('DOMContentLoaded', () => {
+    // Get references to the UI elements
+    const skipRecapSwitch = document.getElementById('skipRecapSwitch');
+    const skipIntroSwitch = document.getElementById('skipIntroSwitch');
+    const nextEpisodeSwitch = document.getElementById('nextEpisodeSwitch');
+    const saveButton = document.getElementById('saveButton');
+  
+    // Load the saved settings and update the UI accordingly
+    browser.storage.sync.get(['skipRecap', 'skipIntro', 'nextEpisode']).then(settings => {
+      skipRecapSwitch.checked = settings.skipRecap || false;
+      skipIntroSwitch.checked = settings.skipIntro || false;
+      nextEpisodeSwitch.checked = settings.nextEpisode || false;
+    });
+  
+    // Save settings when the Save button is clicked
+    saveButton.addEventListener('click', () => {
+
+      console.log('hello world');
+
+      const settings = {
+        skipRecap: skipRecapSwitch.checked,
+        skipIntro: skipIntroSwitch.checked,
+        nextEpisode: nextEpisodeSwitch.checked
+      };
+      browser.storage.sync.set(settings).then(() => {
+        console.log('Settings saved');
+      });
+    });
+  });
+  
