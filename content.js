5cf3fce289be3d87df0979b84b88854fef5617ef
Vishan Sonagadu <vishansonagadu@gmail.com>
2024-10-05 18:51:41 -0400

    Netflix: Skip draining next episode button

--- content.js
+++ content.js
@@ -1,22 +1,26 @@
-
 function clickSkipButton() {
-    // Look for skip element
-    const skipButton = document.querySelector('button[data-uia="player-skip-recap"]') || document.querySelector('button[data-uia="player-skip-intro"]') || document.querySelector('button[data-uia="next-episode-seamless-button"]');
-  
-    if (skipButton) {
-      // Click the "Skip Intro" button if it's found
-      skipButton.click();
-    }
+  // Look for skip element
+  const skipButton =
+    document.querySelector('button[data-uia="player-skip-recap"]') ||
+    document.querySelector('button[data-uia="player-skip-intro"]') ||
+    document.querySelector('button[data-uia="next-episode-seamless-button"]') ||
+    document.querySelector(
+      'button[data-uia="next-episode-seamless-button-draining"]'
+    );
+
+  if (skipButton) {
+    // Click the "Skip Intro" button if it's found
+    skipButton.click();
   }
-  
-  // MutationObserver to monitor DOM changes
-  const observer = new MutationObserver(() => {
-    clickSkipButton();
-  });
-  
-  // Observe the entire document for changes
-  observer.observe(document, { childList: true, subtree: true });
-  
- // In case the button is already on screen
+}
+
+// MutationObserver to monitor DOM changes
+const observer = new MutationObserver(() => {
   clickSkipButton();
-  
+});
+
+// Observe the entire document for changes
+observer.observe(document, { childList: true, subtree: true });
+
+// In case the button is already on screen
+clickSkipButton();
