63ae1bda2fdd8361ce6d826d5d4e7ec06183e283
Vishan Sonagadu <vishansonagadu@gmail.com>
2024-10-08 14:17:46 -0400

    Use local storage instead of shared global variable

--- background.js
+++ background.js
@@ -1,14 +1,30 @@
 // Define a global variable to hold shared data
-let sharedData = { skipIntro: true, skipRecap: true, nextEpisode: true };
+chrome.runtime.onInstalled.addListener(() => {
+  // Set up initial state in chrome.storage if not already set
+  chrome.storage.local.get(['sharedData'], (result) => {
+    if (!result.sharedData) {
+      chrome.storage.local.set({
+        sharedData: { skipIntro: true, skipRecap: true, nextEpisode: true }
+      });
+    }
+  });
+});
 
 // Listener for messages from content or popup scripts
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === 'setSharedData') {
-    // Store data received from content.js
-    sharedData = request.data;
-    sendResponse({ status: 'success', message: 'Data stored in background' });
+    // Store the shared data in chrome.storage.local
+    chrome.storage.local.set({ sharedData: request.data }, () => {
+      sendResponse({ status: 'success', message: 'Data stored in storage' });
+    });
+    // Return true to indicate that sendResponse will be called asynchronously
+    return true;
   } else if (request.action === 'getSharedData') {
-    // Send the stored data to popup.js
-    sendResponse({ data: sharedData });
+    // Retrieve the stored data from chrome.storage.local
+    chrome.storage.local.get(['sharedData'], (result) => {
+      sendResponse({ data: result.sharedData || {} });
+    });
+    // Return true to indicate that sendResponse will be called asynchronously
+    return true;
   }
 });
